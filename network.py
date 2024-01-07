bb1552b22a5d0d494f75add807769bedb64aa894
SlinkierElm5611 <stefan.balta.sb@gmail.com>
2024-01-07 14:54:41 -0500

    Working on back propagation, output layer gradients calculated and working on rest of backprop

--- network.py
+++ network.py
@@ -20,7 +20,10 @@ class Network:
     def __init__(self, layers: list[int]):
         self.layers: list[int] = layers
         self.weights: list[list[list[float]]] = [
-            [[0.5 - random.random() for _ in range(layers[i - 1])] for _ in range(layers[i])]
+            [
+                [0.5 - random.random() for _ in range(layers[i - 1])]
+                for _ in range(layers[i])
+            ]
             for i in range(1, len(layers))
         ]
         self.biases: list[list[list[float]]] = [
@@ -33,8 +36,10 @@ class Network:
     def derivative_squish_function(self, x: float) -> float:
         return math.exp(0.0 - x) / ((1 + math.exp(0.0 - x)) ** 2)
 
-    def feed_forward(self, input: list[list[float]]) -> list[list[float]]:
-        activations: list[list[float]] = [[[value] for row in input for value in row]]
+    def feed_forward(self, input: list[list[float]]) -> list[list[list[float]]]:
+        activations: list[list[list[float]]] = [
+            [[value] for row in input for value in row]
+        ]
         for layer in range(len(self.layers) - 1):
             weights: list[list[float]] = self.weights[layer]
             biases: list[list[float]] = self.biases[layer]
@@ -77,9 +82,33 @@ class Network:
             for i in range(training_labels.dimension_sizes[0])
         ]
 
+    def partial_derivative_cost_function_to_output_layer(
+        self, x: float, y: float
+    ) -> float:
+        return 2 * (x - y)
+
     def back_propagate(
-        self, activations: list[list[float]], label: list[float]
+        self, activations: list[list[list[float]]], label: list[float]
     ) -> tuple[list[list[list[float]]], list[list[list[float]]]]:
+        activation_cost_gradients: list[list[list[float]]] = []
+        weight_cost_gradients: list[list[list[float]]] = []
+        bias_cost_gradients: list[list[list[float]]] = []
+        for layer in range(len(self.layers) - 1):
+            if layer == 0:
+                activation_cost_gradients.append(
+                    [
+                        [self.partial_derivative_cost_function_to_output_layer(x[0], y)]
+                        for x, y in zip(activations[-1], label)
+                    ]
+                )
+            if layer > 0:
+                activation_cost_gradients.append(
+                    [
+                        [sum([value for value in weight_cost_gradients[-layer][i]])]
+                        for i in range(self.layers[-layer])
+                    ]
+                )
+
         return (
             [
                 [
@@ -114,7 +143,7 @@ class Network:
                     label_data[batch * items_per_batch : (batch + 1) * items_per_batch],
                 )
             ):
-                activations: list[list[float]] = self.feed_forward(image)
+                activations: list[list[list[float]]] = self.feed_forward(image)
                 gradients.append(self.back_propagate(activations, label))
             number_of_gradients: int = len(gradients)
             for i in range(len(self.weights)):
@@ -148,7 +177,7 @@ class Network:
         total_correct: int = 0
         total_wrong: int = 0
         for image, label in zip(image_data, label_data):
-            activations: list[list[float]] = self.feed_forward(image)
+            activations: list[list[list[float]]] = self.feed_forward(image)
             if label.index(max(label)) == activations[-1].index(max(activations[-1])):
                 total_correct += 1
             else:
